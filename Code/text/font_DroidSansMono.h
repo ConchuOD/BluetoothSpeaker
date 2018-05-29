6486c4854bdf8b6e1722ce544a7e67a5e6150bae
Conor <?@?.com>
2018-05-29 20:09:42 +0100

    Removing unused fonts - storage rejoices!

--- Code/text/font_DroidSansMono.h
+++ Code/text/font_DroidSansMono.h
@@ -10,24 +10,7 @@
 extern "C" {
 #endif
 
-extern const ILI9341_t3_font_t DroidSansMono_8;
-extern const ILI9341_t3_font_t DroidSansMono_9;
-extern const ILI9341_t3_font_t DroidSansMono_10;
-extern const ILI9341_t3_font_t DroidSansMono_11;
-extern const ILI9341_t3_font_t DroidSansMono_12;
-extern const ILI9341_t3_font_t DroidSansMono_13;
-extern const ILI9341_t3_font_t DroidSansMono_14;
 extern const ILI9341_t3_font_t DroidSansMono_16;
-extern const ILI9341_t3_font_t DroidSansMono_18;
-extern const ILI9341_t3_font_t DroidSansMono_20;
-extern const ILI9341_t3_font_t DroidSansMono_24;
-extern const ILI9341_t3_font_t DroidSansMono_28;
-extern const ILI9341_t3_font_t DroidSansMono_32;
-extern const ILI9341_t3_font_t DroidSansMono_40;
-extern const ILI9341_t3_font_t DroidSansMono_48;
-extern const ILI9341_t3_font_t DroidSansMono_60;
-extern const ILI9341_t3_font_t DroidSansMono_72;
-extern const ILI9341_t3_font_t DroidSansMono_96;
 
 #ifdef __cplusplus
 } // extern "C"
